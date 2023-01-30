17feab3ebee45ab66b037094d22c61b79f9fe52a
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2023-01-30 09:58:52 +0100

    switch to wouter routing and add scss styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Navbar from "./components/navbar/Navbar";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar/>
-      </div>
-    </BrowserRouter>
     
+      <div className="App">
+        
+        <Navbar/>
+        
+      </div>
+   
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import {
-     Routes, Route, Link
-  } from "react-router-dom";
-  import Home from "../../pages/Home";
+import {Link, Route} from "wouter"
+import Home from "../../pages/Home"
 
 
 const Navbar = () => {
     
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
     
+
   return (
-    <Link to={"/"}>123</Link>
+    <div>
+        <Link href="/">
+          Home
+        </Link>
+        <Link href="/test">
+          test
+        </Link>
+
+        <Route path="/">
+          <Home/>
+        </Route>
+    </div>
   )
 }
 
