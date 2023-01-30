af4479540e06ceab9fc18250cc0370fa0d0722c7
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2023-01-30 12:55:53 +0100

    make navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {Link, Route} from "wouter"
 import Home from "../../pages/Home"
-
+import background from "../../assets/images/header-bg.png"
+import phone from "../../assets/images/phone-icon.png"
+import mail from "../../assets/images/mail-icon.png"
+import cart from "../../assets/images/cart-icon.png"
+import { ReactComponent as ArrowIcon } from '../../assets/images/arrow-icon.svg'
 
 const Navbar = () => {
     
@@ -9,16 +13,43 @@ const Navbar = () => {
 
   return (
     <div>
-        <Link href="/">
-          Home
-        </Link>
-        <Link href="/test">
-          test
-        </Link>
+        <nav>
+            <img src={background} alt="nav background" className='nav-bg' />
+            <article className='nav-links'>
+                <Link href="/" className='link-button'>
+                    Forside
+                </Link>
+                <Link href="/test" className='link-button'>
+                    Salgs- og handelsbetingelser
+                </Link>
+                <Link href="/login" className='login-btn'>
+                    Login
+                </Link>
 
-        <Route path="/">
-          <Home/>
-        </Route>
+            </article>
+            <section className='search-field'>
+                <div className='upper-search'>
+                    <div className='upper-search-div'>
+                        <img src={mail} alt="mail-icon" />
+                        <h6>SALES@STRINGSONLINE.COM</h6>
+                    </div>
+                    <div className='upper-search-div'>
+                        <img src={phone} alt="phone-icon" />
+                        <h6>+45 98 12 22 68</h6>
+                    </div>
+                    <div className='upper-search-div'>
+                        <img src={cart} alt="cart-icon" />
+                    </div>
+                </div>
+                <div className='searchbar-cont'>
+                    <input type="text" className='searchbar' placeholder='Indtast søgeord'/>
+                    <button><ArrowIcon/></button>
+                </div>
+            </section>
+        </nav>
+            <Route path="/">
+            <Home/>
+            </Route>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home 123</div>
+    <main>
+      <section className='current-page'>
+        <h5>Forside</h5>
+      </section>
+    </main>
   )
 }
 
