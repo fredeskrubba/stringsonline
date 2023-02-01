4a7339eae073c684ab107f4c08234507a047bf55
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-02-01 12:58:20 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Navbar from "./components/navbar/Navbar";
 import "./styles/output.css"
+import BottomBanner from "./components/bottomBanner/BottomBanner";
 
 function App() {
   return (
     
       <div className="App">
         <Navbar/>
-        
+        <BottomBanner/>
       </div>
    
   );
---
+++ src/components/bottomBanner/BottomBanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import footer from "../../assets/images/footer.png"
+
+const BottomBanner = () => {
+  return (
+    <div className='bottom-banner'>
+        <img src={footer} alt="footer-image" />
+    </div>
+  )
+}
+
+export default BottomBanner
