280226c6584fae6db2fbbbb92ef98bcc2863b341
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-02-01 13:40:10 +0100

    make login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Currentpage from '../components/currentPage/Currentpage'
+import SideNav from '../components/sideNav/SideNav'
 
 const Login = () => {
   return (
     <main>
         <Currentpage/>
-        
+        <div className='login-grid'>
+            <SideNav/>
+            <section className='login-form'>
+                <h2>Login</h2>
+                <p className='subheader'>Indtast brugernavn og adgangskode for at logge på</p>
+                <h4>Brugernavn:</h4>
+                <input type="text" />
+                <h4>Adgangskode:</h4>
+                <input type="password" />
+                <button>Login</button>
+            </section>
+        </div>
     </main>
   )
 }
